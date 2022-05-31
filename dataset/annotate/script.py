ff8ccd92c5ef26b6f6f0fe6d42830faf83850581
vlotusan <vlotusan@gmail.com>
2022-05-31 21:01:36 +0700

    fetch xml data compiled with pandas and convert to csv

--- dataset/annotate/script.py
+++ dataset/annotate/script.py
@@ -1,19 +1,41 @@
 import os
+from xml.dom import xmlbuilder
 import xml.etree.ElementTree as ET
+import pandas as pd
+import glob
 
-tree = ET.parse('(6) - Copy.xml')
+# tree = ET.parse('(6) - Copy.xml')
+# tree.find('./folder').text = 'cobaaja'
+# tree.write('(6) - Copy.xml')
 
-tree.find('./folder').text = 'cobaaja'
+def ubah_xml(path):
+    list_xml = []
+    for file in glob.glob (path + '/*.xml'): #mengecek setiap file .xml
+        tree = ET.parse(file)
+        root = tree.getroot()
+        for object_tag in root.findall('object'): #dikarenakan tiap gambar bisa ada lebih dari 1 objek
+            values = (
+                root[1].text, #filename
+                int(root[4][0].text), #size width
+                int(root[4][1].text), #size height
+                object_tag[0].text, #nama objek
+                int(object_tag[4][0].text), #bndbox xmin
+                int(object_tag[4][1].text), #bndbox ymin
+                int(object_tag[4][2].text), #bndbox xmax
+                int(object_tag[4][3].text) #bndbox ymax
+            )
+            list_xml.append(values)
+    headercsv = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
+    pd_xml = pd.DataFrame(list_xml, columns=headercsv)
+    return pd_xml
 
-tree.write('(6) - Copy.xml')
+    # tree.find('./folder').text = namafolder
+    # # tree.find('./').text =
+    # tree.write(path)
 
+def main():
+    data_xml = ubah_xml(os.getcwd())
+    data_xml.to_csv('labels.csv', index=None) #konversi ke CSV
+    print("Berhasil mengkonversikan xml ke CSV")
 
-def ubah_xml(path, namafolder):
-    tree = ET.parse(path)
-    tree.find('./folder').text = namafolder
-    # tree.find('./').text =
-    tree.write(path)
-
-path1 =
-namafolder1 =
-namapath1 = os.path.join(namafolder1, namafile1)
+main()    
