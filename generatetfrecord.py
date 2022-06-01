f184ac9eab8b4462e7e638d4e8b6f3809c3ed3bb
vlotusan <vlotusan@gmail.com>
2022-06-01 23:20:14 +0700

    Revert "Adding utils from object_detection/utils (tensorflow)"
    This reverts commit 8632e0dfad7fdde5622c5594d59ad28eb4ab739c.

--- generatetfrecord.py
+++ generatetfrecord.py
@@ -4,7 +4,7 @@ import pandas as pd
 import tensorflow as tf
 
 from PIL import Image
-from utils import dataset_util
+from object_detection.utils import dataset_util
 from collections import namedtuple
 
 def split(df, group):
